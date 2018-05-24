2288a8163c3c93273747676b5d5b5ef248bf3e1d
aweoluwatobi <aweoluwatobi26@gmail.com>
2018-05-24 18:46:39 +0100

    Set button to tweet quote

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,26 @@ function randomQuote() {
         dataType: "jsonp",
         data:"method=getQuote&format=jsonp&lang=en&jsonp=?",
         success: function( response ) {
-            $("#random-quote").html('<p class="mb-0" id="random_quote">' +
-            response.quoteText + '</p> <footer class="blockquote-footer author"> '+response.quoteAuthor + '</footer>')
+            $("#random-quote").html('<p class="mb-0" id="random_quote">&quot;' +
+            response.quoteText + '&quot;</p> <footer class="blockquote-footer author"> '+response.quoteAuthor + '</footer>')
         }
     });
 }
+
+function tweet(message) {
+    window.open('https://twitter.com/intent/tweet?hastags=freecodecamp&text=' + encodeURIComponent(message));   
+}
+
+var msg = $("#random-quote");
+function tweetHandler() {
+    tweet($(msg).text());
+}
+
 $(document).ready(function() {
     randomQuote();
     $("#get-quote").on("click", function() {
         randomQuote();
     });
+    $('#send-tweet').on('click', tweetHandler);
 });
+
